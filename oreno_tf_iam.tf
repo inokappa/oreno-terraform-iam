95f2dbde016b8401558a0b1246833d0ba1f7387e
kappa <kappa@kappabookair.local>
2015-07-26 00:00:31 +0900

    modify iam profile name and more.

--- oreno_tf_iam.tf
+++ oreno_tf_iam.tf
@@ -1,16 +1,16 @@
 #
 # Create IAM Instance Profile
 #
-resource "aws_iam_instance_profile" "test_profile" {
-    name = "test_profile"
-    roles = ["${aws_iam_role.test_role.name}"]
+resource "aws_iam_instance_profile" "foo_profile" {
+    name = "foo_profile"
+    roles = ["${aws_iam_role.foo_role.name}"]
 }
 
 #
 # Create IAM Role
 #
-resource "aws_iam_role" "test_role" {
-    name = "test_role"
+resource "aws_iam_role" "foo_role" {
+    name = "foo_role"
     assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -31,9 +31,9 @@ EOF
 #
 # Create IAM Role Policy
 #
-resource "aws_iam_role_policy" "test_policy" {
-    name = "test_policy"
-    role = "${aws_iam_role.test_role.id}"
+resource "aws_iam_role_policy" "foo_policy" {
+    name = "foo_policy"
+    role = "${aws_iam_role.foo_role.id}"
     policy = <<EOF
 {
   "Version": "2012-10-17",
